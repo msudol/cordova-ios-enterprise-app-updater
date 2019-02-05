99acd073eee5aafe9d4e8029f985582e074c9bad
msudol <matt.sudol@gmail.com>
2019-02-05 12:05:42 -0500

    stub out a little more to get organized.

--- src/updater.js
+++ src/updater.js
@@ -2,8 +2,32 @@
 
 class Updater {
     
-    constructor() {
+    // this will be the constructor.
+    constructor(plistURL, appVersion) {
+        this.url = plistURL;
+        this.appVersion = appVersion;
+    }
+    
+    // main method takes success and fail callbacks. 
+    // The success callback will be if the app is up to date.
+    // fail callback returns an error of either, 
+    isUpdated() {
         
     }
+    
+    // async method to query the app.plist wherever it is hosted (using jquery.get) 
+    queryPlist() {
+        
+    }
+    
+    // method to check for update, should return -1 (out of date), 0 (up to date), 1 (somehow newer), or null/error.
+    checkForUpdate() {
+        
+    }
+    
+    // method to check iOS vs Android vs Browser?
+    
+    // method to run the plist version comparison
+    
     
 }
